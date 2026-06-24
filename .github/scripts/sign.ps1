e732eabfa2d10290533ecffae4355058db50c9d4
Silvestr Liskin <silvestr.liskin@teknorob.com>
2026-06-24 09:00:52 +0300

    fix: use signtool from PATH and encode url

--- .github/scripts/sign.ps1
+++ .github/scripts/sign.ps1
@@ -8,7 +8,7 @@ $retryDelaySeconds = 30
 
 for ($i = 0; $i -lt $maxRetries; $i++) {
     Write-Host "Signing attempt $($i + 1) of $maxRetries for $FilePath..."
-    & "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x86\signtool.exe" sign /debug /tr http://ts.ssl.com /td sha256 /fd sha256 /n "Silvestr Liskin" $FilePath
+    signtool.exe sign /debug /tr http://ts.ssl.com /td sha256 /fd sha256 /n "Silvestr Liskin" $FilePath
     
     if ($LASTEXITCODE -eq 0) {
         Write-Host "Signing succeeded!"
